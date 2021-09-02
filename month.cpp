91766f0560cde580160bb3e7f102d6f50a897806
Krina Patel <83603669+Krinap2003@users.noreply.github.com>
2021-09-02 16:24:49 -0400

    Update month.cpp

--- month.cpp
+++ month.cpp
@@ -1,4 +1,11 @@
-/*                                                                                                                     Author: Krina Patel                                                                                                    Course: CSCI: 135                                                                                                      Instructor: Proffesor Mike                                                                                             Assignment: Lab1 part 1                                                                                                */                                                                                                                     #include <iostream>                                                                                                    //using namespace std
+/*                                                                                                  
+   Author: Krina Patel
+   Course: CSCI: 135
+   Instructor: Proffesor Mike
+   Assignment: Lab1 part D
+*/    
+#include <iostream> 
+
 int main()
 {
   int year;
@@ -9,7 +16,22 @@ int main()
   std::cin>>month;
   if(month == 2)
     {
-      if(year % 4 != 0)                                                                                                         {                                                                                                                         std::cout <<"28 days "<<std::endl;                                                                                 }                                                                                                                     else if(year % 100 != 0)                                                                                                  {                                                                                                                         std::cout<<"29 days"<<std::endl;                                                                                    }                                                                                                                     else if(year % 400 != 0)                                                                                                  {                                                                                                                         std::cout<<"28 days"<< std::endl;                                                                                 }                                                                                                                     else                                                                                                                      {                                                                                                                         std::cout<< "29 days"<<std::endl;                                                                                   }   
+      if(year % 4 != 0)   
+      { 
+        std::cout <<"28 days "<<std::endl;
+      }
+     else if(year % 100 != 0)
+     {
+       std::cout<<"29 days"<<std::endl;
+     }
+     else if(year % 400 != 0)
+     {
+       std::cout<<"28 days"<< std::endl;
+     }
+     else  
+     {
+       std::cout<< "29 days"<<std::endl; 
+     }
     }
   else if(month % 2 == 0)
     {
