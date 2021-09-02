9a93fe4088a7b770d273c1bc6c897fa642d12563
krina <krinap@DESKTOP-DLKQ5RN.localdomain>
2021-09-02 16:02:05 -0400

    add leap.cpp

---
+++ leap.cpp
@@ -0,0 +1,30 @@
+/*                                                                                                                  
+   Author: Krina Patel                                                                                              
+   Course: CSCI: 135                                                                                                
+   Instructor: Proffesor Mike                                                                                       
+   Assignment: Lab1 part 1                                                                                        
+*/                                                                                   
+#include <iostream>
+
+int main()
+{
+  int year;                                                                                                           std::cout<<"Enter year: ";                                                                                          std::cin>>year;
+
+  if(year % 4 != 0)
+    {
+      std::cout <<"Common year"<<std::endl;
+    }
+  else if(year % 100 != 0)
+    {
+      std::cout<<"Leap year"<<std::endl;
+    }
+  else if(year % 400 != 0)
+    {
+      std::cout<<"Common year"<< std::endl;
+    }
+  else
+    {
+      std::cout<< "Leap year"<<std::endl;
+    }
+  return 0;
+}
